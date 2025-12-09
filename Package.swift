9c44aadd83804bf865ae1ec0ab6db574025ac0c2
kenneth.ljh <see2851@gmail.com>
2025-12-09 15:24:08 +0800

    feat: Update package details and raise iOS deployment target
    - Rename package and library from "AlicloudUTDID" to "UTDID".
    - Increase the minimum iOS deployment target from v10 to v12.
    - Update the binary target name to "UTDID" to align with the new package name.

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "AlicloudUTDID",
+    name: "UTDID",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v12)
     ],
     products: [
         .library(
-            name: "AlicloudUTDID",
-            targets: ["AlicloudUTDID"]
+            name: "UTDID",
+            targets: ["UTDID"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "AlicloudUTDID",
+            name: "UTDID",
             url: "https://github.com/KennethLaw/AlicloudUTDID/releases/download/1.6.1/UTDID.xcframework.zip",
             checksum: "e67370a145fa1f4b0c65c6e884df61b2da3250efa8752ecacd9467107c1f4cf3"
         )
